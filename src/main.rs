4c6ff161c248c070d52b7f2e99a21c33c0306964
Al Madireddy <al.madireddy@gmail.com>
2020-03-18 22:02:41 -0500

    read yaml from file and display a selctor

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 extern crate clap;
+extern crate dialoguer;
+extern crate yaml_rust;
+extern crate dirs;
+
 use clap::{Arg, App, SubCommand};
+use dialoguer::{theme::ColorfulTheme, Select};
+use yaml_rust::{YamlLoader, Yaml};
+use std::fs::{File};
+use std::io::{Read, Error, ErrorKind};
+use std::vec::Vec;
 
 fn main() {
     let matches = App::new("Projects")
@@ -23,17 +32,94 @@ fn main() {
                         .index(2)))
         .get_matches();
 
-    if let Some(matches) = matches.subcommand_matches("open") {
-        open_project(matches);
-    } else if let Some(_matches) = matches.subcommand_matches("list") {
-        list_projects();
-    } else if let Some(matches) = matches.subcommand_matches("add") {
-        add_project(matches);
+    match matches.subcommand() {
+        ("open", Some(sub_command)) => open_project(&sub_command), 
+        ("list", Some(_subc)) => list_projects(), 
+        ("add", Some(sub_command)) => add_project(&sub_command), 
+        (&_, _) => println!("{}", matches.usage())
+    }    
+}
+
+fn load_projects_from_data() -> std::result::Result<Yaml, Error> {
+    let home_directory = dirs::home_dir();
+    let mut config_path: std::path::PathBuf;
+    let f: std::io::Result<File>;
+
+    match home_directory {
+        Some(home) => {
+            config_path = home.clone();
+            config_path.push(".projectman");
+            f = File::open(config_path.clone());
+        }, 
+        None => {
+            println!("Error getting home directory");
+            return Err(Error::new(ErrorKind::Other, "failed to find home directory"));
+        },
+    }
+
+    match f {
+        Ok(mut contents) => {
+            let mut s = String::new();
+            contents.read_to_string(&mut s)?;
+            let docs = YamlLoader::load_from_str(&s).unwrap();
+            if docs.len() == 0 {
+                return Err(Error::new(ErrorKind::Other, "No yaml in file, may be corrupted"))
+            }
+            return Ok(docs[0].clone())
+        }, 
+        Err(e) => {
+            if let Some(raw_err) = e.raw_os_error() {
+                match raw_err {
+                    2 => {
+                        println!("file doesn't exist, creating");
+                        std::fs::File::create(config_path)?;
+                        let v: Yaml = Yaml::from_str("");
+                        return Ok(v)
+                    },
+                    _ => {
+                        println!("Other error reading data file");
+                        return Err(e)  
+                    } 
+                }
+            } else {
+                return Err(e)
+            }
+        } 
     }
 }
 
 fn list_projects() {
-    println!("listing projects");
+    let projects_yaml = load_projects_from_data();
+    let projects: Yaml; 
+
+    match projects_yaml {
+        Ok(pr) => {
+            projects = pr.clone();
+            println!("{:?}", projects);
+        },
+        Err(r) => {
+            println!("error: {}", r);
+            return
+        },
+    }
+
+    let mut hash = projects.clone().into_hash().unwrap();
+    let mut keys: Vec<String> = Vec::new();
+
+    for entry in hash.entries() {
+        let key = entry.key().as_str().unwrap().to_owned();
+        keys.push(key);
+    }
+
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Pick your flavor")
+        .default(0)
+        .items(keys.as_slice())
+        .interact()
+        .unwrap();
+    println!("selection: {}", selection);
+    let choice = keys.get(selection).unwrap();
+    println!("Enjoy your {:?}!", projects[choice.as_str()]);
 }
 
 fn add_project(matches: &clap::ArgMatches<'_>) {
